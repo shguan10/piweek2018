82465e346779f309ab3d9df8b3c46451b0199ab9
EC2 Server <server@server.com>
2018-03-23 04:21:11 +0000

    Setup backend with postgres

---
+++ backend/startup.sh
@@ -0,0 +1,19 @@
+# Migrations
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+# Create Superuser for Admin Site
+cat <<EOF | python3 manage.py shell
+from django.contrib import auth
+User = auth.get_user_model()
+User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+EOF
+
+# Start the server
+echo "Starting server"
+exec uwsgi --master \
+           --socket /tmp/snowflake.sock \
+           --chmod-socket=666 \
+	   --workers 4 \
+	   --static-map /static=/home/ubuntu/backend/static \
+	   --wsgi-file /home/ubuntu/backend/backend/wsgi.py
