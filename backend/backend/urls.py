00206a04c2549144746997eed6cd1be5a7a59c46
William Zhang <wz2@andrew.cmu.edu>
2018-03-24 00:27:32 -0400

    Modified URLs and viewsets

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,7 +3,8 @@ from django.http import HttpRequest
 from django.shortcuts import render
 
 from rest_framework import routers
-from snowflake.viewset import FridgeViewSet
+from snowflake.fridge_viewset import FridgeViewSet
+from snowflake.auth_viewset import SignupViewSet, SigninViewSet
 from . import admin
 
 router = routers.SimpleRouter()
@@ -16,6 +17,8 @@ def render_index(request: HttpRequest):
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^', include(router.urls)),
+    re_path(r'^signup/$', SignupViewSet.as_view()),
+    re_path(r'^signin/$', SigninViewSet.as_view()),
     re_path(r'^api/v1/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     re_path(r'^/', render_index)
 ]
