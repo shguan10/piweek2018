4e22a808c0836cd1c72bce184d39a7ba07701c27
William Zhang <wz2@andrew.cmu.edu>
2018-03-23 23:46:21 -0400

    Implemented index.html

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,15 +1,21 @@
 from django.urls import path, re_path, include
+from django.http import HttpRequest
+from django.shortcuts import render
+
 from rest_framework import routers
 from snowflake.viewset import FridgeViewSet
 from . import admin
 
-
 router = routers.SimpleRouter()
 router.register(r'api/fridge', FridgeViewSet, base_name='Fridge')
+
+
+def render_index(request: HttpRequest):
+    return render(request, "./index.html")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^', include(router.urls)),
-    re_path(r'^api/v1/api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-    # re_path(r'^(?!media).*$', renderer.render_index)
+    re_path(r'^api/v1/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    re_path(r'^/', render_index)
 ]
