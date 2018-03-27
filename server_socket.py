65fd80ec20f8e61df570d285ff80d8c81e7a8957
EC2 Server <server@server.com>
2018-03-27 02:24:06 +0000

    Server Socket

---
+++ server_socket.py
@@ -0,0 +1,46 @@
+import io
+import socket
+import _thread
+import struct
+import tempfile
+import datetime
+import pathlib
+
+
+def on_new_client(conn, addr):
+    connection = conn.makefile('rb')
+    directory = tempfile.TemporaryDirectory()
+    user_id = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+    print(directory)
+    print(user_id)
+
+    while True:
+        # Read the length of the image as a 32-bit unsigned int. If the length is zero, quit the loop
+        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+        if not image_len:
+            break
+            
+        dt = datetime.datetime.now()
+        file = directory.name + "/" + dt.strftime("%s")
+               
+        # Construct a stream to hold the image data and read the image data from the connection
+        with open(file, 'wb') as file_stream:
+            file_stream.write(connection.read(image_len))
+
+    connection.close()
+    while True:
+        pass
+
+
+# Start a socket listening for connections on 0.0.0.0:8000
+server_socket = socket.socket()
+server_socket.bind(('0.0.0.0', 8000))
+server_socket.listen(0)
+
+try:
+    while True:
+        conn, addr = server_socket.accept()
+        _thread.start_new_thread(on_new_client, (conn, addr))
+        
+finally:
+    server_socket.close()
